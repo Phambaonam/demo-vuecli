057989f1137052442e1fc6787cae956f4a826510
Phambaonam <phambaonam1312@gmail.com>
2017-07-24 17:48:54 +0700

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1 v-if="$route.params.id">{{$route.params.id}}</h1>
+    <h1 v-else>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Chào mừng các bạn đến với Vue.js',
+    };
+  },
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
